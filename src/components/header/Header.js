010201fc16aa95cc2832abb1ad80234398eb1edc
SanjarKeySkills <forest2004@mail.ru>
2023-07-14 22:34:04 +0600

    add hero compo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,12 +4,11 @@ import Img2 from "../../img/burger-symbols_menu.svg";
 import "./header.css";
 
 const Header = () => {
-
     return (
         <div className="container">
             <div className="header__main">
                 <div className="header__img">
-                    <img className="header__logo" src={Img1} alt="logo"/>
+                    <img className="header__logo" src={Img1} alt="logo" />
                 </div>
                 <div className="header__info">
                     <div className="header__contacts">
@@ -18,8 +17,8 @@ const Header = () => {
                     </div>
                     <div>
                         <button className="burger__btn">
-                            <span className="burger__title-btn">Menu</span>
-                                <img src={Img2} alt="logo-btn"/>
+                            <span className="burger__title-btn">메뉴</span>
+                            <img src={Img2} alt="logo-btn" />
                         </button>
                     </div>
                 </div>
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,11 +4,21 @@ import "./hero.css";
 const hero = () => {
     return (
         <div className="container">
-            <div className="hero__img">
+            <div className="hero__img-wrapper">
                 <div className="hero__items">
-                    <div className="hero__h1"></div>
-                    <div className="hero__text"></div>
-                    <div className="hero__button"></div>
+                    <div className="hero__title">
+                        <h1>놀라운 여행 일정</h1>
+                        {/* Удивительный туристический маршрут */}
+                    </div>
+                    <div className="hero__text-wrapper">
+                        <p className="hero__text">
+                            대한민국의 귀빈이 되십시오. 키르기스스탄의 산악
+                            유목민
+                        </p>
+                        {/* Стань почетным гостем из Республики Корея в
+                                горном кочевом мире Кыргызстана */}
+                    </div>
+                    <button className="hero__button">투어 예약</button>
                 </div>
             </div>
         </div>
