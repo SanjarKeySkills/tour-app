b2afaee86280b2dcdbce8a33bbfe432ad1910f3a
SanjarKeySkills <forest2004@mail.ru>
2023-07-14 17:36:42 +0600

    create header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
-// import logo from "../img/한국인 관광객.svg";
+import Img1 from "../../img/images_2.png";
+import Img2 from "../../img/burger-symbols_menu.svg";
 import "./header.css";
 
 const Header = () => {
 
     return (
         <div className="container">
-            <div>Hello world</div>
             <div className="header__main">
                 <div className="header__img">
-                    {/*<img src={logo} alt="logo" />*/}
+                    <img className="header__logo" src={Img1} alt="logo"/>
                 </div>
                 <div className="header__info">
                     <div className="header__tel">+86 45 86 46 056</div>
                     <div className="header__mail">email@gmail.com</div>
                     <div>
-                        <button className="burger__btn">Menu</button>
+                        <button className="burger__btn">
+                            <span className="burger__title-btn">Menu</span>
+                                <img src={Img2} alt="logo-btn"/>
+                        </button>
                     </div>
                 </div>
             </div>
